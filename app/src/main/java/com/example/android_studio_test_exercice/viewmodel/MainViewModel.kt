e4057b39c3994a14b1602e333c6837bdd04074b4
XavierFarrus <xavier.farrus@gracia.lasalle.cat>
2026-04-28 22:21:40 +0200

    Main y viewmodel

--- app/src/main/java/com/example/android_studio_test_exercice/viewmodel/MainViewModel.kt
+++ app/src/main/java/com/example/android_studio_test_exercice/viewmodel/MainViewModel.kt
@@ -1,14 +1,10 @@
 package com.example.android_studio_test_exercice.viewmodel
-import androidx.compose.runtime.getValue
-import androidx.compose.runtime.mutableStateOf
-import androidx.compose.runtime.saveable.rememberSaveable
-import androidx.compose.runtime.setValue
 import androidx.compose.ui.state.ToggleableState
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 
-class MainViewModel: ViewModel {
+class MainViewModel : ViewModel() {
     // Atributs
     private val _estatSwitch: MutableLiveData<Boolean>
     public val estatSwitch: LiveData<Boolean>
@@ -28,11 +24,29 @@ class MainViewModel: ViewModel {
     private val _selectedOption: MutableLiveData<String>
     public val selectedOption: LiveData<String>
 
+    private val _sliderValue: MutableLiveData<Float>
+    public val sliderValue: LiveData<Float>
+
+    private val _expanded: MutableLiveData<Boolean>
+    public val expanded: LiveData<Boolean>
+
+    private val _selectedItem: MutableLiveData<String>
+    public val selectedItem: LiveData<String>
+
+    private val _searchText: MutableLiveData<String>
+    public val searchText: LiveData<String>
+
+    private val _showSnackbar: MutableLiveData<Boolean>
+    public val showSnackbar: LiveData<Boolean>
+
+    private val _toggleState: MutableLiveData<Boolean>
+    public val toggleState: LiveData<Boolean>
+
     /**
      * Constructor de la classe HelloViewModel
      * que inicialitzen els atributs
      */
-    constructor() : super() {
+    init {
         this._estatSwitch = MutableLiveData<Boolean>(true)
         this.estatSwitch = this._estatSwitch
 
@@ -50,18 +64,44 @@ class MainViewModel: ViewModel {
 
         this._selectedOption = MutableLiveData<String>("Messi")
         this.selectedOption = this._selectedOption
+
+        this._sliderValue = MutableLiveData<Float>(0f)
+        this.sliderValue = this._sliderValue
+
+        this._expanded = MutableLiveData<Boolean>(false)
+        this.expanded = this._expanded
+
+        this._selectedItem = MutableLiveData<String>("Opció A")
+        this.selectedItem = this._selectedItem
+
+        this._searchText = MutableLiveData<String>("")
+        this.searchText = this._searchText
+
+        this._showSnackbar = MutableLiveData<Boolean>(false)
+        this.showSnackbar = this._showSnackbar
+
+        this._toggleState = MutableLiveData<Boolean>(false)
+        this.toggleState = this._toggleState
     }
 
-    fun toggleEstatSwitch(){
+    fun toggleEstatSwitch() {
         this._estatSwitch.value = !(this._estatSwitch.value)!!
     }
 
-    fun toggleEsCarnivor(){
+    fun toggleEsVegetaria() {
+        this._esVegetaria.value = !(this._esVegetaria.value)!!
+    }
+
+    fun toggleEsVega() {
+        this._esVega.value = !(this._esVega.value)!!
+    }
+
+    fun toggleEsCarnivor() {
         this._esCarnivor.value = !(this._esCarnivor.value)!!
     }
 
-    fun toggleTriStateStatus(){
-        when(this._triStateStatus.value){
+    fun toggleTriStateStatus() {
+        when (this._triStateStatus.value) {
             ToggleableState.On -> setTriStateStatus(ToggleableState.Off)
             ToggleableState.Off -> setTriStateStatus(ToggleableState.Indeterminate)
             ToggleableState.Indeterminate -> setTriStateStatus(ToggleableState.On)
@@ -69,7 +109,35 @@ class MainViewModel: ViewModel {
         }
     }
 
-    private fun setTriStateStatus(triState: ToggleableState){
+    fun setSelectedOption(option: String) {
+        this._selectedOption.value = option
+    }
+
+    fun setSliderValue(value: Float) {
+        this._sliderValue.value = value
+    }
+
+    fun setExpanded(isExpanded: Boolean) {
+        this._expanded.value = isExpanded
+    }
+
+    fun setSelectedItem(item: String) {
+        this._selectedItem.value = item
+    }
+
+    fun setSearchText(text: String) {
+        this._searchText.value = text
+    }
+
+    fun performSearch() {
+        this._showSnackbar.value = this._searchText.value?.isNotBlank() == true
+    }
+
+    fun toggle() {
+        this._toggleState.value = !(this._toggleState.value ?: false)
+    }
+
+    private fun setTriStateStatus(triState: ToggleableState) {
         this._triStateStatus.value = triState
     }
 }
